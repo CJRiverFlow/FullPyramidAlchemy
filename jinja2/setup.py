9c8af513e92a8f6642d6b788715d14f19bf093cc
Christian Rivera <cjosue.flores@gmail.com>
2021-03-25 03:44:17 +0000

    SQL tests

---
+++ jinja2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+# List of dependencies installed via `pip install -e .`
+# by virtue of the Setuptools `install_requires` value below.
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'jinja2',
+    'waitress',
+]
+
+dev_requires = [
+    'pyramid_debugtoolbar',
+    'pytest',
+    'webtest',
+]
+
+setup(
+    name='tutorial',
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires,
+    },
+    entry_points={
+        'paste.app_factory': [
+            'main = tutorial:main'
+        ],
+    },
+)
---
+++ jinja2/tutorial/__init__.py
@@ -0,0 +1,13 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    #templating engine
+    config.include('pyramid_jinja2')
+    #routes
+    config.add_route('home', '/')
+    config.add_route('hello', '/howdy')
+    #views
+    config.scan('.views')
+    return config.make_wsgi_app()
